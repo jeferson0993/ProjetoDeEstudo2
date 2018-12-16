316b77fafb87f67431fee63fcf36d30f2ebd1ee1
Jeferson Ferreira <jeferson0993@gmail.com>
2018-12-16 18:16:42 +0000

    Criado os arquivos index.php, topo.php e main.css. Criado a pasta imagems, adicionado os arquivos fundo_topo.jpg e logo.png. Criado a area do topo do site em arquivo separado e inserido no arquivo index por meio do include. Deletado o arquivo index.html;

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Projeto Site Completo</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />    
+</head>
+<body>
+    <header>
+       <?php
+        include "topo.php";
+       ?> 
+    </header>
+</body>
+<script src="main.js"></script>
+</html>
---
+++ topo.php
@@ -0,0 +1,9 @@
+<div id="logo_topo">
+
+</div>
+<nav id="menu">    
+    <a class="botao_menu" href="index.php" target="_self">HOME</a>
+    <a class="botao_menu" href="index.php" target="_self">PRODUTOS</a>
+    <a class="botao_menu" href="index.php" target="_self">CONTATO</a>
+    <a class="botao_menu" href="index.php" target="_self">LOCALIZAÇÃO</a>
+</nav>
